50cf493551bce9009e4c75913daca4e143770bca
loren1994 <dayan805@163.com>
2018-12-25 13:59:32 +0800

    升级flutter

--- lib/pages/Person.dart
+++ lib/pages/Person.dart
@@ -3,6 +3,20 @@ import 'package:flutter/material.dart';
 class Person extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Text("我的页面");
+    return Column(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.all(20),
+          child: FlatButton(
+              child: Text("按钮1"),
+              color: Colors.orangeAccent,
+              highlightColor: Colors.orangeAccent[700],
+              colorBrightness: Brightness.dark,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)),
+              onPressed: () => {}),
+        )
+      ],
+    );
   }
 }
