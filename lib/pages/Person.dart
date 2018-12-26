405087b408875a513902ed89bf0e91da60057d3a
loren1994 <dayan805@163.com>
2018-12-26 16:06:33 +0800

    offstage切换

--- lib/pages/Person.dart
+++ lib/pages/Person.dart
@@ -1,22 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
-class Person extends StatelessWidget {
+class Person extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return VisLayout();
+  }
+}
+
+class VisLayout extends State<Person> {
+  bool vis = false;
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-          margin: EdgeInsets.all(20),
-          child: FlatButton(
-              child: Text("按钮1"),
-              color: Colors.orangeAccent,
-              highlightColor: Colors.orangeAccent[700],
-              colorBrightness: Brightness.dark,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20)),
-              onPressed: () => {}),
-        )
-      ],
-    );
+    return Stack(children: <Widget>[
+      Offstage(
+        offstage: vis,
+        child: Column(
+          children: <Widget>[
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.only(top: 100),
+              child: InkWell(
+                onTap: () => this.setState(() {
+                      vis = true;
+                    }),
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(colors: [
+                        Colors.red,
+                        Colors.orange,
+                        Colors.orangeAccent
+                      ]),
+                      borderRadius: BorderRadius.circular(3),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.grey,
+                            offset: Offset(2, 2),
+                            blurRadius: 3)
+                      ]),
+                  child: Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 120, vertical: 10),
+                    child: Text(
+                      "登录",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.only(top: 16),
+              child: RaisedButton(
+                  color: Colors.orangeAccent,
+                  highlightColor: Colors.orangeAccent[700],
+                  onPressed: () => Fluttertoast.showToast(msg: "注册"),
+                  colorBrightness: Brightness.dark,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5)),
+                  child: Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 105, vertical: 10),
+                    child: Text("注册"),
+                  )),
+            ),
+          ],
+        ),
+      ),
+      Offstage(
+        offstage: !vis,
+        child: RaisedButton(
+          onPressed: () => this.setState(() {
+                vis = false;
+              }),
+          child: Text("test text"),
+        ),
+      )
+    ]);
   }
 }
